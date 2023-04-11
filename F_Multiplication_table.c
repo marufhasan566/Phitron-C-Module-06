874721d5141fc79cd4b73bb450a45da9fe7685b8
marufhasan566 <marufhasan.mh1@gmail.com>
2023-04-11 10:49:44 +0600

    f. multiplication table added

---
+++ F_Multiplication_table.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int N;
+    scanf("%d",&N);
+    for(int i =1;i<=12;i++){
+        int result = N*i;
+        printf("%d * %d = %d\n",N,i,result);
+    }
+    return 0;
+}
