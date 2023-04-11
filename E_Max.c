91b4c512d293d4affe326054e4d00dc2046e50ff
marufhasan566 <marufhasan.mh1@gmail.com>
2023-04-11 09:47:44 +0600

    minimum value added

--- E_Max.c
+++ E_Max.c
@@ -1,17 +1,22 @@
 #include<stdio.h>
+#include<limits.h>
 int main()
 {
     int N;
     scanf("%d",&N);
     int A;
-    int Max = 0;
+    int Max = INT_MIN, Min=INT_MAX;
     for(int i=0;i<N;i++){
         scanf("%d",&A);
         // printf("%d\n",A);
         if(A>Max){
             Max=A;   
         }
+        // if(A<Min){
+        //     Min=A;
+        // }
     }
     printf("%d\n",Max);
+    // printf("%d\n",Min);
     return 0;
 }
